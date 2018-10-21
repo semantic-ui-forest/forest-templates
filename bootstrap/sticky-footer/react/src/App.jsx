d10d6f3421700a0de97f0b3ac3ea9aa9bb82f07c
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-10-21 21:54:35 +0800

    Add react version for bootstrap/sticky-footer

---
+++ bootstrap/sticky-footer/react/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import "semantic-ui-css/semantic.min.css";
+
+import {
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Segment
+} from "semantic-ui-react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Grid container>
+          <Grid.Column>
+            <Header as="h1" size="huge">
+              Sticky footer
+            </Header>
+            <Divider />
+            <p className="lead">
+              Pin a fixed-height footer to the bottom of the viewport in desktop
+              browsers with this custom HTML and CSS.
+            </p>
+          </Grid.Column>
+        </Grid>
+        <Segment basic className="footer">
+          <Container>
+            <p>Place sticky footer content here.</p>
+          </Container>
+        </Segment>
+      </div>
+    );
+  }
+}
+
+export default App;
