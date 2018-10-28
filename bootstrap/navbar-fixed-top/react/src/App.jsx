04b3f1acc79fbb65cd77437056182977016897bf
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-10-28 12:33:15 +0800

    Add react version of bootstrap/navbar-fixed-top template

---
+++ bootstrap/navbar-fixed-top/react/src/App.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+import "semantic-ui-css/semantic.min.css";
+
+import {
+  Button,
+  Container,
+  Dropdown,
+  Header,
+  Menu,
+  Message
+} from "semantic-ui-react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Menu borderless fluid size="big" fixed="top">
+          <Container>
+            <Menu.Item header>Project Name</Menu.Item>
+            <Menu.Item active>Home</Menu.Item>
+            <Menu.Item>About</Menu.Item>
+            <Menu.Item>Contact</Menu.Item>
+            <Dropdown item text="Dropdown">
+              <Dropdown.Menu>
+                <Dropdown.Item as="a" href="#root">
+                  Action
+                </Dropdown.Item>
+                <Dropdown.Item as="a" href="#root">
+                  Another Action
+                </Dropdown.Item>
+                <Dropdown.Item as="a" href="#root">
+                  Something else here
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item as="a" href="#root">
+                  Separated link
+                </Dropdown.Item>
+                <Dropdown.Item as="a" href="#root">
+                  One more separated link
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Container>
+        </Menu>
+        <Container id="content">
+          <Message>
+            <Header size="huge" as="h1">
+              Navbar example
+            </Header>
+            <p className="lead">
+              This example is a quick exercise to illustrate how the default,
+              static navbar and fixed to top navbar work. It includes the
+              responsive CSS and HTML, so it also adapts to your viewport and
+              device.
+            </p>
+            <Button primary size="big" as="a">
+              View navbar docs &raquo;
+            </Button>
+          </Message>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
