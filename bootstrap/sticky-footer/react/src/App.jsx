b576322a1cf39d936c3ddd09bfb89ae8c1c1972d
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-12-04 22:17:26 +0800

    Align footer content, and use dividing header

--- bootstrap/sticky-footer/react/src/App.jsx
+++ bootstrap/sticky-footer/react/src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import "semantic-ui-css/semantic.min.css";
 
-import { Container, Divider, Grid, Header, Segment } from "semantic-ui-react";
+import { Container, Grid, Header, Segment } from "semantic-ui-react";
 
 import "./App.css";
 
@@ -12,17 +12,16 @@ class App extends Component {
       <div className="App">
         <Grid container>
           <Grid.Column>
-            <Header as="h1" size="huge">
+            <Header dividing as="h1" size="huge">
               Sticky footer
             </Header>
-            <Divider />
             <p className="lead">
               Pin a fixed-height footer to the bottom of the viewport in desktop
               browsers with this custom HTML and CSS.
             </p>
           </Grid.Column>
         </Grid>
-        <Segment basic className="footer" as="footer">
+        <Segment basic as="footer">
           <Container>
             <p>Place sticky footer content here.</p>
           </Container>
