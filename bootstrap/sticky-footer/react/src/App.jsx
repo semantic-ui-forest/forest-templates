896a53d5ea4cde09646cc51bb4db06061cd32b80
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-12-04 23:09:57 +0800

    Revise page structure

--- bootstrap/sticky-footer/react/src/App.jsx
+++ bootstrap/sticky-footer/react/src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import "semantic-ui-css/semantic.min.css";
 
-import { Container, Grid, Header, Segment } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
 
 import "./App.css";
 
@@ -10,22 +10,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Grid container>
-          <Grid.Column>
-            <Header dividing as="h1" size="huge">
-              Sticky footer
-            </Header>
-            <p className="lead">
-              Pin a fixed-height footer to the bottom of the viewport in desktop
-              browsers with this custom HTML and CSS.
-            </p>
-          </Grid.Column>
-        </Grid>
-        <Segment basic as="footer">
+        <Container>
+          <Header dividing as="h1" size="huge">
+            Sticky footer
+          </Header>
+          <p className="lead">
+            Pin a fixed-height footer to the bottom of the viewport in desktop
+            browsers with this custom HTML and CSS.
+          </p>
+        </Container>
+        <footer>
           <Container>
             <p>Place sticky footer content here.</p>
           </Container>
-        </Segment>
+        </footer>
       </div>
     );
   }
