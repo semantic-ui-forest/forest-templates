79a79842263829ba822e310b0beea3e53b992008
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-12-04 23:29:43 +0800

    Pixel fix to bootstrap/sticky-footer-navbar, also add responsive navbar

--- bootstrap/sticky-footer-navbar/react/src/App.jsx
+++ bootstrap/sticky-footer-navbar/react/src/App.jsx
@@ -3,63 +3,120 @@ import React, { Component } from "react";
 import "semantic-ui-css/semantic.min.css";
 
 import {
-  Divider,
+  Button,
+  Container,
   Dropdown,
   Grid,
   Header,
-  Menu,
-  Segment
+  Icon,
+  Menu
 } from "semantic-ui-react";
 
 import "./App.css";
 
 class App extends Component {
+  state = {
+    dropdownMenuStyle: {
+      display: "none"
+    }
+  };
+
+  handleToggleDropdownMenu = () => {
+    let newState = Object.assign({}, this.state);
+    if (newState.dropdownMenuStyle.display === "none") {
+      newState.dropdownMenuStyle = { display: "flex" };
+    } else {
+      newState.dropdownMenuStyle = { display: "none" };
+    }
+
+    this.setState(newState);
+  };
+
   render() {
     return (
       <div className="App">
-        <Menu borderless fixed="top" size="huge">
-          <Grid container>
-            <Grid.Row>
+        <Grid padded className="tablet computer only">
+          <Menu borderless fixed="top" size="huge">
+            <Container>
               <Menu.Item header as="a">
                 Project Name
               </Menu.Item>
-              <Menu.Item active>Home</Menu.Item>
-              <Menu.Item>About</Menu.Item>
-              <Menu.Item>Contact</Menu.Item>
+              <Menu.Item active as="a">
+                Home
+              </Menu.Item>
+              <Menu.Item as="a">About</Menu.Item>
+              <Menu.Item as="a">Contact</Menu.Item>
               <Dropdown text="Dropdown" className="item">
                 <Dropdown.Menu>
-                  <Dropdown.Item>Action</Dropdown.Item>
-                  <Dropdown.Item>Another action</Dropdown.Item>
-                  <Dropdown.Item>Something else here</Dropdown.Item>
-                  <Divider />
+                  <Dropdown.Item as="a">Action</Dropdown.Item>
+                  <Dropdown.Item as="a">Another action</Dropdown.Item>
+                  <Dropdown.Item as="a">Something else here</Dropdown.Item>
+                  <Dropdown.Divider />
                   <Dropdown.Header>Navbar header</Dropdown.Header>
-                  <Dropdown.Item>Seperated link</Dropdown.Item>
-                  <Dropdown.Item>One more seperated link</Dropdown.Item>
+                  <Dropdown.Item as="a">Seperated link</Dropdown.Item>
+                  <Dropdown.Item as="a">One more seperated link</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
-            </Grid.Row>
-          </Grid>
-        </Menu>
-        <Grid container>
-          <Grid.Column>
-            <Header as="h1" size="huge">
-              Sticky footer with fixed navbar
-            </Header>
-            <Divider />
-            <p className="lead">
-              Pin a fixed-height footer to the bottom of the viewport in desktop
-              browsers with this custom HTML and CSS. A fixed navbar has been
-              added with "ui fixed menu".
-            </p>
-          </Grid.Column>
+            </Container>
+          </Menu>
         </Grid>
-        <Segment basic className="footer">
-          <Grid container>
-            <Grid.Row>
-              <p>Place sticky footer content here.</p>
-            </Grid.Row>
-          </Grid>
-        </Segment>
+        <Grid className="mobile only">
+          <Menu borderless size="huge" fixed="top">
+            <Menu.Item header as="a">
+              Project Name
+            </Menu.Item>
+            <Menu.Menu position="right">
+              <Menu.Item>
+                <Button
+                  icon
+                  basic
+                  toggle
+                  onClick={this.handleToggleDropdownMenu}
+                >
+                  <Icon name="content" />
+                </Button>
+              </Menu.Item>
+            </Menu.Menu>
+            <Menu
+              vertical
+              borderless
+              fluid
+              style={this.state.dropdownMenuStyle}
+            >
+              <Menu.Item active as="a">
+                Home
+              </Menu.Item>
+              <Menu.Item as="a">About</Menu.Item>
+              <Menu.Item as="a">Contact</Menu.Item>
+              <Dropdown text="Dropdown" className="item">
+                <Dropdown.Menu>
+                  <Dropdown.Item as="a">Action</Dropdown.Item>
+                  <Dropdown.Item as="a">Another action</Dropdown.Item>
+                  <Dropdown.Item as="a">Something else here</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Navbar header</Dropdown.Header>
+                  <Dropdown.Item as="a">Seperated link</Dropdown.Item>
+                  <Dropdown.Item as="a">One more seperated link</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu>
+          </Menu>
+        </Grid>
+        <Container>
+          <Header dividing as="h1" size="huge">
+            Sticky footer with fixed navbar
+          </Header>
+          <p className="lead">
+            Pin a fixed-height footer to the bottom of the viewport in desktop
+            browsers with this custom HTML and CSS. A fixed navbar has been
+            added with "ui fixed menu".
+          </p>
+        </Container>
+        <footer>
+          <Container>
+            <p>Place sticky footer content here.</p>
+          </Container>
+        </footer>
       </div>
     );
   }
