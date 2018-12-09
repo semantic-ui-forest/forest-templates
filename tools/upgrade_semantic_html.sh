bb63cb849fd4c7e96dd8078183ce96decc8525a5
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-12-09 12:08:01 +0800

    Revise tools/upgrade_semantic_html.sh

--- tools/upgrade_semantic_html.sh
+++ tools/upgrade_semantic_html.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-version=$1
+old_version=$1
+new_version=$2
 
-for html in bootstrap/*/html/index.html
+for html in bootstrap/*/html/index.html semantic-ui/*/html/index.html
 do
-    echo -n "upgrade $html with semantic-ui $version ..."
-    sed -i '' "s/semantic-ui\/2.3.3\/semantic.min.css/semantic-ui\/$version\/semantic.min.css/" $html
-    sed -i '' "s/semantic-ui\/2.3.3\/semantic.min.js/semantic-ui\/$version\/semantic.min.js/" $html
+    echo -n "upgrade $html with semantic-ui from $old_version to $new_version ..."
+    sed -i '' "s/semantic-ui\/$old_version\/semantic.min.css/semantic-ui\/$new_version\/semantic.min.css/" $html
+    sed -i '' "s/semantic-ui\/$old_version\/semantic.min.js/semantic-ui\/$new_version\/semantic.min.js/" $html
     echo " done"
 done
