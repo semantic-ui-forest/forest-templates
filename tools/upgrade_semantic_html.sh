6f23cb6f89f7b95b89bd287700ea5ea78567a75a
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-12-09 12:05:47 +0800

    Upgrade semantic-ui.min.js version from 2.3.3 to 2.4.1

--- tools/upgrade_semantic_html.sh
+++ tools/upgrade_semantic_html.sh
@@ -2,9 +2,10 @@
 
 version=$1
 
-for html in bootstrap/*/html/index.html semantic-ui/*/html/index.html
+for html in bootstrap/*/html/index.html
 do
     echo -n "upgrade $html with semantic-ui $version ..."
     sed -i '' "s/semantic-ui\/2.3.3\/semantic.min.css/semantic-ui\/$version\/semantic.min.css/" $html
+    sed -i '' "s/semantic-ui\/2.3.3\/semantic.min.js/semantic-ui\/$version\/semantic.min.js/" $html
     echo " done"
 done
