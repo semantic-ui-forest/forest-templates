c9332b6876892d34d11ab2cbff4da4a0e4267524
Xiao Hanyu <xiaohanyu1988@gmail.com>
2019-02-22 22:47:08 +0800

    reimplement react upgrade script in js

---
+++ scripts/upgrade-semantic-react.js
@@ -0,0 +1,18 @@
+const _ = require('log-timestamp');
+
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+function main() {
+  for (let category of ["bootstrap", "semantic-ui"]) {
+    for (let template of fs.readdirSync(category)) {
+      console.log(`Upgrade ${category}/${template} react to latest version...`);
+      execSync("yarn upgrade --latest semantic-ui-react", {
+        cwd: path.join(category, template, "react")
+      })
+    }
+  }
+}
+
+main()
