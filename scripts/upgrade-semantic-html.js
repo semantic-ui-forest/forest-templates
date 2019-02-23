d77dc29fa7097bab76ff148be39b5e58f72227d8
Xiao Hanyu <xiaohanyu1988@gmail.com>
2019-02-23 08:31:19 +0800

    Reimplement sh in js script to upgrade semantic-ui.min.css version

---
+++ scripts/upgrade-semantic-html.js
@@ -0,0 +1,41 @@
+const _ = require("log-timestamp");
+
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+function main() {
+  if (process.argv.length !== 4) {
+    process.stderr.write("wrong number of arguments.\n");
+    process.stderr.write(
+      "node upgrade-html-react.js oldSemanticUIVersion newSemanticUIVersion\n"
+    );
+    process.exit(-1);
+  }
+
+  let content, srcHtmlPath, destHtmlPath;
+
+  const oldSemanticUIVersion = process.argv[2];
+  const newSemanticUIVersion = process.argv[3];
+
+  for (let category of ["bootstrap", "semantic-ui"]) {
+    for (let template of fs.readdirSync(category)) {
+      console.log(
+        `Upgrade ${category}/${template} html semantic to from` +
+        ` ${oldSemanticUIVersion} to ${newSemanticUIVersion}...`
+      );
+      srcHtmlPath = path.join(category, template, "html/index.html");
+      destHtmlPath = path.join(category, template, "html/index.html");
+      content = fs.readFileSync(srcHtmlPath, "utf-8");
+      fs.writeFileSync(
+        destHtmlPath,
+        content.replace(
+          `semantic-ui/${oldSemanticUIVersion}/semantic.min.css`,
+          `semantic-ui/${newSemanticUIVersion}/semantic.min.css`
+        )
+      );
+    }
+  }
+}
+
+main();
